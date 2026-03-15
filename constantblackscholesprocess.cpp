17b3d6d57c6f9a2f695bca926e4ae0d8e96c88ae
quentin <quentin.assie2@gmail.com>
2026-03-15 18:55:55 +0100

    correction des âneries d'Anais sur ConstantBSM cpp et hpp

--- constantblackscholesprocess.cpp
+++ constantblackscholesprocess.cpp
@@ -1,35 +1,35 @@
-#include "constantblackscholesprocess.hpp"
+#include "ConstantBlackScholesProcess.hpp"
 
 using namespace QuantLib;
 
-constantblackscholesprocess::ConstantBlackScholesProcess(Real S0, Rate r, Rate q, Volatility sigma){
+ConstantBlackScholesProcess::ConstantBlackScholesProcess(Real S0, Rate r, Rate q, Volatility sigma){
 	S0_ = S0;
 	r_ = r;
 	q_ = q;
 	sigma_ = sigma;
 }
 
-constantblackscholesprocess::~ConstantBlackScholesProcess(){
+ConstantBlackScholesProcess::~ConstantBlackScholesProcess(){
 	
 }
 
-Real drift(Time t, Real S) const{
+//derivée du modele
+Real ConstantBlackScholesProcess::drift(Time t, Real S) const { 
 	//S est le prix du sous-jacent au temps t
 
 	return (r_ - q_) * S;
 }
 
-Real diffusion(Time t, Real S) const{
-	//S est le prix du sous-jacent au temps t
+//partie aléatoire du modèle
+Real ConstantBlackScholesProcess::diffusion(Time t, Real S) const {
 
 	return sigma_ * S;
 }
 
-Real evolve(Time t0, Real S0, Time dt, Real dw) const{
-	//S est le prix du sous-jacent au temps t0
+Real ConstantBlackScholesProcess::evolve(Time t0, Real X0, Time dt, Real dw) const{
+	//X0 est le prix du sous-jacent au temps t0
 	//dw est un tirage gaussien
 
-	return S0 * exp((r_- q_ - 0.5*sigma_*sigma_)*dt + sigma_*sqrt(dt)*dw); 
+	return X0 * exp((r_- q_ - 0.5 * sigma_* sigma_) * dt + sigma_* sqrt(dt) * dw) ; //resultat SDE BlackSholesModel
 }
 
- "constantblackscholesprocess.hpp"
